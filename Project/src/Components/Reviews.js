a39f910c71d885c59c826b66e22436efc79ff941
Abhay Madaan <78895271+AbhayMadaan9@users.noreply.github.com>
2022-08-04 16:51:33 +0530

    commiting changes

---
+++ Project/src/Components/Reviews.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './reviews.css'
+export default function Reviews() {
+  return (
+    <>
+    <div className="container-fluid">
+        <h2>What our Customer Says</h2>
+
+    </div>
+    </>
+  )
+}
