7dfb824e03ecc5bdf62586cfd10db0ebaa94f386
Abhay Madaan <78895271+AbhayMadaan9@users.noreply.github.com>
2022-08-05 12:26:56 +0530

    commit the changes

--- Project/src/Components/Reviews.js
+++ Project/src/Components/Reviews.js
@@ -3,8 +3,54 @@ import './reviews.css'
 export default function Reviews() {
   return (
     <>
-    <div className="container-fluid">
-        <h2>What our Customer Says</h2>
+    <div className="container-fluid" id='container1'>
+        <div className="heading">
+        <h3>What our Customer Says</h3>
+        </div>
+        <div className="container-fluid" id='container2'>
+          <div className="carousel-slide" data-ride='carousel' id='mycarousel'>
+            <ol className='carousel-indicators'>
+              <li data-target='mycarousel' data-ride-to='0' className='active'></li>
+              <li data-target='mycarousel' data-ride-to='0'></li>
+              <li data-target='mycarousel' data-ride-to='0'></li>
+            </ol>
+
+            <div className="carousel-inner" role='listbox'>
+              <div className="carousel-item active">
+                <div className="card-wrapper">
+                  <div className="card">
+                    <div className="card-body">
+                      <div className="card-text">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil odit itaque nesciunt, eum commodi eos necessitatibus adipisci, modi sint provident iste accusantium alias quisquam maiores ex perferendis, eius quo optio!</p> 
+                      </div>
+                      <img src="https://mamaearthp.imgix.net/wysiwyg/Priyanshi_Singh_gHqZqXk.jpg?auto=format" alt="customer img" className='img-circle'/>
+                      <strong>Ramesh Rani</strong> <span style={{color:'white', background:'#00ff7f'}}>4.6<i class="fa-solid fa-star" style={{color:'white'}}></i></span>
+                    </div>
+                  </div>
+                  <div className="card">
+                    <div className="card-body">
+                      <div className="card-text">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil odit itaque nesciunt, eum commodi eos necessitatibus adipisci, modi sint provident iste accusantium alias quisquam maiores ex perferendis, eius quo optio!</p> 
+                      </div>
+                      <img src="https://mamaearthp.imgix.net/wysiwyg/Priyanshi_Singh_gHqZqXk.jpg?auto=format" alt="customer img" className='img-circle'/>
+                      <strong>Ramesh Rani</strong> <span style={{color:'white', background:'#00ff7f'}}>4.6<i class="fa-solid fa-star" style={{color:'white'}}></i></span>
+                    </div>
+                  </div>
+                  <div className="card">
+                    <div className="card-body">
+                      <div className="card-text">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil odit itaque nesciunt, eum commodi eos necessitatibus adipisci, modi sint provident iste accusantium alias quisquam maiores ex perferendis, eius quo optio!</p> 
+                      </div>
+                      <img src="https://mamaearthp.imgix.net/wysiwyg/Priyanshi_Singh_gHqZqXk.jpg?auto=format" alt="customer img" className='img-circle'/>
+                      <strong>Ramesh Rani</strong> <span style={{color:'white', background:'#00ff7f'}}>4.6<i class="fa-solid fa-star" style={{color:'white'}}></i></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+       
 
     </div>
     </>
