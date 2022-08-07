ee12ad0d614e827d6c1df78e39e7457a2fadf809
Abhay Madaan <78895271+AbhayMadaan9@users.noreply.github.com>
2022-08-07 09:56:10 +0530

    make changes

--- Project/src/Components/Footer1_0.js
+++ Project/src/Components/Footer1_0.js
@@ -4,39 +4,59 @@ export default function Footer1_0() {
   return (
     <>
     <div className="container">
-      <table>
+      <div className="row">
+        <div className="col-lg-8">
+        <table>
         
-          <tr>
-            <th>USEFUL LINKS</th>
-            <th>CATEGORIES</th>
-            <th>MY ACCOUNT</th>
-          </tr>
+        <tr>
+          <th>USEFUL LINKS</th> 
+          <th>CATEGORIES</th>
+          <th>MY ACCOUNT</th>
+        </tr>
 
 
-          <tr>
-            <td>Privacy Policy</td>
-            <td>Baby</td>
-            <td>Account</td>
-          </tr>
+        <tr>
+          <td><a href='#'>Privacy Policy</a></td>
+          <td><a href="#">Baby</a></td>
+          <td><a href="#">Account</a></td>
+        </tr>
 
-          <tr>
-            <td>Returns</td>
-            <td>Beauty</td>
-            <td>Orders</td>
-          </tr>
+        <tr>
+          <td><a href="#">Returns</a></td>
+          <td><a href="#">Beauty</a></td>
+          <td><a href="#">Orders</a></td>
+        </tr>
 
-          <tr>
-            <td>Terms & Conditions</td>
-            <td>Hair</td>
-            <td>Addresses</td>
-          </tr>
+        <tr>
+          <td>Terms & Conditions</td>
+          <td>Hair</td>
+          <td>Addresses</td>
+        </tr>
 
-          <tr>
-            <td>Terms& Condiotions-Cashback</td>
-            <td>Face</td>
-          </tr>
-      </table>
+        <tr>
+          <td>Terms& Condiotions-Cashback</td>
+          <td>Face</td>
+        </tr>
+
+    </table>
+
+        </div>
+        <div className="col-lg-4">
+        <h3>Have Queries Or Concerns</h3>
+           <button className='btn'>Contact Us</button>
+        </div>
       </div>
+         </div>
+         <div className="container">
+          <h3>FOLLOW US SOCIAL MEDIA</h3>
+          <p><i class="fa-brands fa-facebook fa-xl"></i>
+          <i class="fa-brands fa-twitter fa-xl"></i>
+          <i class="fa-brands fa-linkedin-in fa-xl"></i>
+          <i class="fa-brands fa-instagram fa-xl"></i>
+          <i class="fa-brands fa-youtube fa-xl"></i>
+          </p>
+          <p style={{margin:'20px auto auto auto', textAlign:'center'}}>Copyright 2022 Kushal Medical Hall, All right reserved</p>
+         </div>
     </>
   )
 }
